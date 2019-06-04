85358b0c45e9af64d394f91544ee51c5d29b601f
fumi232323 <fumie.tanaka@beproud.jp>
2019-06-04 22:38:26 +0900

    Nikola auto commit.
    Nikola version: 8.0.2

--- conf.py
+++ conf.py
@@ -17,7 +17,7 @@ import time
 
 # Data about this site
 BLOG_AUTHOR = "fumi23"  # (translatable)
-BLOG_TITLE = "fuminote"  # (translatable)
+BLOG_TITLE = "ふみのて"  # (translatable)
 # This is the main URL for your site. It will be used
 # in a prominent link. Don't forget the protocol (http/https)!
 SITE_URL = "https://32imuf.com/"
